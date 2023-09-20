08e3d318183593a63ebfc18a51f4f2d37075d287
benton-tripp <btripp@ncsu.edu>
2023-09-20 08:53:30 -0400

    testing NA some more...

--- test/ipp_testing.R
+++ test/ipp_testing.R
@@ -8,6 +8,7 @@ library(terra)
 library(sf)
 library(data.table)
 library(purrr)
+library(ggplot2)
 
 # Load pre-processed (training) data
 df.train <- readRDS("test/df.train.rds")
@@ -41,16 +42,12 @@ get.range <- function(r, .crs=4326) {
   )
 }
 
-# Get raster bounds (lon=xmin & max, lat=ymin & max)
-r.range <- get.range(r)
-
-
-
 # Ensure counter-clockwise direction
 is.ccw <- function(p) {
   tryCatch({owin(poly=p); T}, error=function(e) F)
 }
 
+# Check that all polygons were traversed in the right direction
 ensure.ccw <- function(polygon.list) {
   lapply(polygon.list, function(p) {
     # Check the first polygon (outer boundary)
@@ -90,29 +87,35 @@ convert.to.list.format <- function(sf.object) {
 }
 
 # Get the first layer, set it to either NA or TRUE
-r.poly <- terra::project(x=r[[1]], 
+r.poly <- terra::project(x=sum(r, na.rm=F), 
                  y=st_crs(4326, parameters=T)$Wkt) %>% 
   lapp(function(z) ifelse(is.na(z), NA, T)) %>%
   terra::as.polygons() %>%
-  st_as_sf() %>%
-  convert.to.list.format()
+  # Convert to polygon
+  st_as_sf()
 
-# Error in owin(poly = r.poly[[2]]) : Area of window is negative;
-# check that all polygons were traversed in the right direction
+# Convert polygon to list
+r.poly.list <- convert.to.list.format(r.poly)
+
+# Convert your point dataframe to an sf object
+ipp.sf <- st_as_sf(ipp.df, coords = c("lon", "lat"), crs = 4326)
+
+# Get indices of points that are within the polygon
+valid.pts <- sapply(st_intersects(ipp.sf, r.poly), function(x) length(x) > 0)
 
 # Convert the data to a ppp object
-locations <- ppp(ipp.df$lon, ipp.df$lat, 
-                 poly=r.poly) 
+locations <- ppp(ipp.df$lon[valid.pts], ipp.df$lat[valid.pts], 
+                 poly=r.poly.list) 
 
 # Limit raster layers to those variables selected in during 
 # Feature selection (during EDA), and with importance >= 1
 fs.df <- fs.df %>%
   filter(importance >= 1)
 
+# Get list of distinct variable names
 r.fs <- c(fs.df$var1, fs.df$var2) %>% 
   unique() %>% 
   sort()
-
 
 # Define min/max scaling function for rasters
 min.max.scale <- function(r, na.rm=T) {
@@ -120,6 +123,15 @@ min.max.scale <- function(r, na.rm=T) {
   max.r <- max(values(r), na.rm=na.rm)
   (r - min.r) / (max.r- min.r)
 }
+
+# Update each layer based to have NA if any layer is NA
+r <- names(r) %>%
+  set_names() %>%
+  purrr::map(function(n) {
+  layer <- r[[i]]
+  layer[is.na(sum(r))] <- NA
+  return(layer)
+})
 
 # Load/compute filtered & pre-processed rasters
 ipp.raster.dir <- "artifacts/ipp_rasters"
@@ -162,6 +174,27 @@ r.funcs <- names(r.ipp) %>%
     f
   })
 
+test.extrct <- map_df(1:locations$n, function(i) {
+  x <- locations$x[[i]]
+  y <- locations$y[[i]]
+  val.df <- names(r.ipp) %>%
+    set_names() %>%
+    purrr::map_df(~r.funcs[[.x]](x, y, r.ipp, .x))
+  cbind(data.frame(x=x, y=y), val.df)
+})
+faulty.indices <- which(is.na(test.extrct), arr.ind=T) 
+faulty.pts <- test.extrct[faulty.indices[,1], c("x", "y")]
+faulty.pts %>% as_tibble()
+# [1] x y
+# <0 rows> (or 0-length row.names)
+
+faulty.pts.sf <- st_as_sf(faulty.pts, coords = c("x", "y"), crs = st_crs(r.poly))
+ggplot() +
+  geom_sf(data = r.poly, fill = "lightblue") +                # Plotting the polygon
+  geom_sf(data = faulty.pts.sf, color = "red", size = 2.5) +  # Overlaying the points
+  theme_minimal()
+
+# Define covfunargs using raster list names
 .covfunargs <- set_names(names(r.ipp)) %>% as.list()
 .covfunargs$raster.list <- r.ipp
 # Create formula
@@ -171,6 +204,23 @@ r.funcs <- names(r.ipp) %>%
 
 # Fit point-process model
 # TODO: Address the problem with NA values in the `locations` variable
-# How come, even after setting a polygon boundary
+# How come some of the points are apperently outside of the bounds, even
+# after taking measures to ensure they are within the bounds? I even 
+# checked extracting all of the values from the rasters
 fit <- ppm(Q=.f, covariates=r.funcs, covfunargs=.covfunargs, method="mpl")
 
+na.pts.idx <- which(fit$problems$na.covariates$quadpoints.na)
+fit$internal$glmdata[na.pts.idx, ]
+
+loc.df <- data.frame(x=c(locations$x, fit$Q$dummy$x), 
+                     y=c(locations$y, fit$Q$dummy$y))
+faulty.df2 <- loc.df[na.pts.idx, ] %>% as_tibble()
+
+faulty.pts.sf.2 <- st_as_sf(faulty.df2, coords = c("x", "y"), crs = st_crs(r.poly))
+bb <- st_bbox(faulty.pts.sf.2)
+ggplot() +
+  geom_sf(data = r.poly, fill = "tan") +                # Plotting the polygon
+  geom_sf(data = faulty.pts.sf.2, color = "red", size = 2.5) +  # Overlaying the points
+  theme_minimal() +
+  coord_sf(xlim = c(bb["xmin"], bb["xmax"]), ylim = c(bb["ymin"], bb["ymax"]))
+  
