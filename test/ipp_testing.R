7b2122cc714b7c3f3dd5abe4443dad3623cd617f
benton-tripp <btripp@ncsu.edu>
2023-09-20 09:52:10 -0400

    fitting my own dummies

--- test/ipp_testing.R
+++ test/ipp_testing.R
@@ -14,6 +14,17 @@ library(ggplot2)
 df.train <- readRDS("test/df.train.rds")
 # Load pre-processed rasters (covariates)
 r <- rast("test/NC_covariates.tif")
+
+# Update each layer based to have NA if any layer is NA
+r <- names(r) %>%
+  set_names() %>%
+  purrr::map(function(n) {
+    layer <- r[[i]]
+    layer[is.na(sum(r))] <- NA
+    return(layer)
+  }) %>%
+  rast()
+
 # Load feature selection results from EDA
 fs.df <- readRDS("test/fs.df.rds")
 
@@ -21,11 +32,11 @@ fs.df <- readRDS("test/fs.df.rds")
 st <- "NC"
 spec <- "Sharp-shinned Hawk"
 
-# IPP does not use absence points; filter by state & species
+# filter by state & species
 ipp.df <- df.train %>% 
-  filter(presence == 1 & state == st & common.name == spec) %>%
+  filter(state == st & common.name == spec) %>%
   # select location point
-  select(lon, lat) %>% 
+  select(lon, lat, presence) %>% 
   # Get the unique points, since I am not accounting for the temporal nature of the data
   unique() 
 
@@ -103,9 +114,21 @@ ipp.sf <- st_as_sf(ipp.df, coords = c("lon", "lat"), crs = 4326)
 # Get indices of points that are within the polygon
 valid.pts <- sapply(st_intersects(ipp.sf, r.poly), function(x) length(x) > 0)
 
-# Convert the data to a ppp object
-locations <- ppp(ipp.df$lon[valid.pts], ipp.df$lat[valid.pts], 
+# Filter out invalid points
+ipp.df <- filter(ipp.df, valid.pts)
+# Subset df by presence
+ipp.presence <- filter(ipp.df, presence == 1)
+ipp.dummies <- filter(ipp.df, presence == 0)
+
+# Convert the data to a ppp objects
+locations <- ppp(ipp.presence$lon, 
+                 ipp.presence$lat, 
                  poly=r.poly.list) 
+dummy.locs <- ppp(ipp.dummies$lon, 
+                  ipp.dummies$lat, 
+                  poly=r.poly.list) 
+
+Q <- quadscheme(locations, dummy.locs)
 
 # Limit raster layers to those variables selected in during 
 # Feature selection (during EDA), and with importance >= 1
@@ -123,15 +146,6 @@ min.max.scale <- function(r, na.rm=T) {
   max.r <- max(values(r), na.rm=na.rm)
   (r - min.r) / (max.r- min.r)
 }
-
-# Update each layer based to have NA if any layer is NA
-r <- names(r) %>%
-  set_names() %>%
-  purrr::map(function(n) {
-  layer <- r[[i]]
-  layer[is.na(sum(r))] <- NA
-  return(layer)
-})
 
 # Load/compute filtered & pre-processed rasters
 ipp.raster.dir <- "artifacts/ipp_rasters"
@@ -174,26 +188,6 @@ r.funcs <- names(r.ipp) %>%
     f
   })
 
-test.extrct <- map_df(1:locations$n, function(i) {
-  x <- locations$x[[i]]
-  y <- locations$y[[i]]
-  val.df <- names(r.ipp) %>%
-    set_names() %>%
-    purrr::map_df(~r.funcs[[.x]](x, y, r.ipp, .x))
-  cbind(data.frame(x=x, y=y), val.df)
-})
-faulty.indices <- which(is.na(test.extrct), arr.ind=T) 
-faulty.pts <- test.extrct[faulty.indices[,1], c("x", "y")]
-faulty.pts %>% as_tibble()
-# [1] x y
-# <0 rows> (or 0-length row.names)
-
-faulty.pts.sf <- st_as_sf(faulty.pts, coords = c("x", "y"), crs = st_crs(r.poly))
-ggplot() +
-  geom_sf(data = r.poly, fill = "lightblue") +                # Plotting the polygon
-  geom_sf(data = faulty.pts.sf, color = "red", size = 2.5) +  # Overlaying the points
-  theme_minimal()
-
 # Define covfunargs using raster list names
 .covfunargs <- set_names(names(r.ipp)) %>% as.list()
 .covfunargs$raster.list <- r.ipp
@@ -207,20 +201,85 @@ ggplot() +
 # How come some of the points are apperently outside of the bounds, even
 # after taking measures to ensure they are within the bounds? I even 
 # checked extracting all of the values from the rasters
-fit <- ppm(Q=.f, covariates=r.funcs, covfunargs=.covfunargs, method="mpl")
+fit <- ppm(Q=Q, trend=.f, covariates=r.funcs, covfunargs=.covfunargs, method="mpl")
+# Warning message:
+#   Values of the covariates ‘emergent_herbaceous_wetlands’, ‘avg_prcp’, ‘lon.sqrd’, 
+# ‘shrubland’, ‘developed’, ‘herbaceous’, ‘max.ndvi’, ‘mixed_forest’, ‘barren’, ‘temp.diff’,
+# ‘water’, ‘min.ndvi’, ‘woody_wetlands’, ‘lat.sqrd’, ‘dem.detrended’, ‘deciduous_forest’,
+# ‘evergreen_forest’, ‘coastline’ were NA or undefined at 0.49% (15 out of 3032) of the
+# quadrature points. Occurred while executing: ppm.ppp(Q = locations, trend =
+# ~emergent_herbaceous_wetlands +  
+
 
 na.pts.idx <- which(fit$problems$na.covariates$quadpoints.na)
-fit$internal$glmdata[na.pts.idx, ]
-
 loc.df <- data.frame(x=c(locations$x, fit$Q$dummy$x), 
                      y=c(locations$y, fit$Q$dummy$y))
-faulty.df2 <- loc.df[na.pts.idx, ] %>% as_tibble()
+loc.df[na.pts.idx, ] %>% as_tibble()
 
-faulty.pts.sf.2 <- st_as_sf(faulty.df2, coords = c("x", "y"), crs = st_crs(r.poly))
-bb <- st_bbox(faulty.pts.sf.2)
+faulty.pts <- st_as_sf(loc.df[na.pts.idx, ], 
+                            coords = c("x", "y"), 
+                            crs = st_crs(r.poly))
+intersects.check <- st_intersects(faulty.pts, r.poly)
+outside.pts <- sapply(intersects.check, length) == 0
+# All of them are true
+
+# Try fitting the model with the window
+W <- owin(poly = r.poly.list)
+fit <- ppm(Q = .f, Window = W, covariates = r.funcs, 
+           covfunargs = .covfunargs, method = "mpl")
+# Still get warning
+# Warning message:
+#   Values of the covariates ‘emergent_herbaceous_wetlands’, ‘avg_prcp’, ‘lon.sqrd’, 
+# ‘shrubland’, ‘developed’, ‘herbaceous’, ‘max.ndvi’, ‘mixed_forest’, ‘barren’, ‘temp.diff’,
+# ‘water’, ‘min.ndvi’, ‘woody_wetlands’, ‘lat.sqrd’, ‘dem.detrended’, ‘deciduous_forest’,
+# ‘evergreen_forest’, ‘coastline’ were NA or undefined at 0.49% (15 out of 3032) of the
+# quadrature points. Occurred while executing: ppm.ppp(Q = locations, trend =
+# ~emergent_herbaceous_wetlands +  
+
+
+bb <- st_bbox(faulty.pts)
 ggplot() +
-  geom_sf(data = r.poly, fill = "tan") +                # Plotting the polygon
-  geom_sf(data = faulty.pts.sf.2, color = "red", size = 2.5) +  # Overlaying the points
+  geom_sf(data = r.poly, fill = "tan") +  
+  geom_sf(data = faulty.pts, color = "red", size = 2.5) +  
   theme_minimal() +
   coord_sf(xlim = c(bb["xmin"], bb["xmax"]), ylim = c(bb["ymin"], bb["ymax"]))
   
+
+# visual inspection shows that the points are indeed outside of the boundaries of the polygon. I
+# have double checked spatial extents and projections, and that doesn't appear to be the issue.
+# And my code also shows that the `r.func` functions are working fine, extracting onlyy valid 
+# points in my test. The issue seems to be how `ppm` is getting dummy locations.
+
+
+# Generate a dense grid over the bounding box
+bb <- as(st_bbox(r.poly), "bbox")
+grid.pts <- expand.grid(x = seq(bb[1], bb[3], by = 0.01),  # adjust the step size as needed
+                        y = seq(bb[2], bb[4], by = 0.01))
+
+# Convert grid_points to sf object
+grid.sf <- st_as_sf(grid.pts, coords = c("x", "y"), crs = 4326)
+
+# Keep only those points within the study region
+inside.pts <- st_within(grid.sf, r.poly)
+inside.pt.mask <- sapply(inside.pts, length) > 0
+inside.grid.sf <- grid.sf[inside.pt.mask,]
+
+# Convert these inside_points to a ppp object for spatstat
+inside.ppp <- as(inside.grid.sf, "ppp")
+
+new.dummy <- default.dummy(locations, npix=c(nrow(r), ncol(r)), verbose=T)
+
+dummy.df <- data.frame(x=new.dummy$x, 
+                       y=new.dummy$y)
+
+dummy.df <- st_as_sf(dummy.df, 
+                       coords = c("x", "y"), 
+                       crs = st_crs(r.poly))
+intersects.check <- st_intersects(dummy.df, r.poly)
+outside.pts <- sapply(intersects.check, length) == 0
+
+# Fit the model using this new point pattern
+fit <- ppm(locations_with_quad, trend=.f, covariates=r.funcs, 
+           covfunargs=.covfunargs, method="mpl")
+
+
